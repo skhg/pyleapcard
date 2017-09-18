ef6dc254d99b27feab568c6d26b6b290f4ba8c8c
skhg <git@jackhiggins.ie>
2017-09-18 23:53:40 +0100

    adding some further management/config stuff
    i’ve submitted it to PyPi but it obviously doesn’t work yet

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(name='pyleapcard',
       version='0.1.0',
       description='Python Leap Card API',
       author='Jack Higgins',
+      author_email='pypi@jackhiggins.ie',
       url='https://github.com/skhg/pyleapcard',
       py_modules=['pyleapcard'],
+      license='MIT'
       )
