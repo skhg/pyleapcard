8565961f1313770c6619f6f0082be887ac315e85
Jack <git@jackhiggins.ie>
2020-06-22 18:19:05 +0200

    include the readme in the description

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pyleapcard',
