e5d96091345ea4465da7342fb05ddf356960b171
Jack Higgins <1479627+skhg@users.noreply.github.com>
2020-06-22 18:30:36 +0200

    Nicer description info (#14)
    * include the readme in the description
    
    * include the readme in the description
    
    * longer basic description
    
    * use other setup
    
    * remove utf8

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='pyleapcard',
     version='0.0.0',
-    description='Python Leap Card API',
-    long_description='See project page with usage examples at https://github.com/skhg/pyleapcard',
+    description="Python Leap Card API (Unofficial). Access your card balance and journey history for Ireland's public "
+                "transport smart card.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords='leap card public transport smart card api travel ireland',
     author='Jack Higgins',
     author_email='pypi@jackhiggins.ie',
